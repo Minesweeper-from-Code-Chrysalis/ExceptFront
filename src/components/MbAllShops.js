8fff5b9bb407df35c09ea8895659112206442b0c
y3-kang <26410322+regent0ro@users.noreply.github.com>
2020-12-02 10:08:36 +0900

    [Update] Modify styles

--- src/components/MbAllShops.js
+++ src/components/MbAllShops.js
@@ -1,24 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Card, CardContent, CardMedia, Grid, Typography } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import TrainIcon from "@material-ui/icons/Train";
 import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 import DirectionsWalkIcon from "@material-ui/icons/DirectionsWalk";
+import NoImage from "../images/no_image.png";
 
 const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   details: {
     // display: 'flex',
     // flexDirection: 'column',
   },
   content: {
-    flex: '1 0 auto',
-    marginTop: '-10px',
-    textAlign: 'left',
-    color: 'white',
+    flex: "1 0 auto",
+    marginTop: "-10px",
+    textAlign: "left",
+    color: "white",
   },
   cover: {
     height: 100,
@@ -46,41 +47,39 @@ export default function MbAllShops(props) {
         onKeyDown={() => {
           setSelectedShop(shop);
         }}
-        style={{paddingBottom: "4px"}}
+        style={{ paddingBottom: "4px" }}
       >
-        <Card className={classes.root} style={{ backgroundColor: "#383737", height: "100%"}}>
-          <CardMedia className={classes.cover} image={shop.image_url.shop_image1} title="shop_image" />
+        <Card className={classes.root} style={{ backgroundColor: "#383737", height: "100%" }}>
+          <CardMedia className={classes.cover} image={!shop.image_url.shop_image1 ? NoImage : shop.image_url.shop_image1} title="shop_image" />
           <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <div style={{overflow: "hidden", textOverflow: "ellipsis", width: '14rem', fontSize: 12}}> 
-            <Typography variant="body2" component="body2" 
-            // noWrap
-            >
-              {shop.name}
-            </Typography>
-            <Grid container direction="row" alignItems="center" justify="left" spacing={0}>
-              <Grid item>
-                <TrainIcon fontSize="small"/>
-              </Grid>
-              <Grid>
-                {shop.access.station}
-              </Grid>
-              <Grid item>
-                <DirectionsWalkIcon fontSize="small" />
-              </Grid>
-              <Grid>
-                {shop.access.walk}
-                {"分"}
-              </Grid>
-              <Grid item>
-                <MonetizationOnIcon fontSize="small" />
-              </Grid>
-              <Grid>
-                {shop.budget}円
-              </Grid>
-            </Grid>
-            </div>
-          </CardContent>
+            <CardContent className={classes.content}>
+              <div style={{ overflow: "hidden", textOverflow: "ellipsis", width: "14rem", fontSize: 12 }}>
+                <Typography
+                  variant="body2"
+                  component="body2"
+                  // noWrap
+                >
+                  {shop.name}
+                </Typography>
+                <Grid container direction="row" alignItems="center" justify="left" spacing={0}>
+                  <Grid item>
+                    <TrainIcon fontSize="small" />
+                  </Grid>
+                  <Grid>{shop.access.station}</Grid>
+                  <Grid item>
+                    <DirectionsWalkIcon fontSize="small" />
+                  </Grid>
+                  <Grid>
+                    {shop.access.walk}
+                    {"分"}
+                  </Grid>
+                  <Grid item>
+                    <MonetizationOnIcon fontSize="small" />
+                  </Grid>
+                  <Grid>{shop.budget}円</Grid>
+                </Grid>
+              </div>
+            </CardContent>
           </div>
         </Card>
       </Grid>
@@ -88,7 +87,7 @@ export default function MbAllShops(props) {
   });
 
   return (
-   <div style={{padding:"5px", backgroundColor: "#252627"}}>
+    <div style={{ padding: "5px", backgroundColor: "#252627" }}>
       <Grid container item xs={12} justify="center">
         {items}
       </Grid>
